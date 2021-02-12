68b24c60825b632cbe53679dda77da4c9feccd91
Allen Palacios <apalacios1994@gmail.com>
2021-02-12 19:58:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_fav_colors = ["blue", "black", "white", "green", "arrays!"]
-  my_fav_colors.pop("arrays!")
+  my_array = my_fav_colors.pop("arrays!") 
 end
 
 def remove_element_from_start_of_array(array)
